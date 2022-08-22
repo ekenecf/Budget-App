43d6031a3ba21a5aa39a7244c314a482531abb8b
ekenecf <ekens2u@gmail.com>
2022-08-22 21:19:29 +0100

    Create controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :groups, foreign_key: 'user_id', dependent: :destroy
+  has_many :deals, foreign_key: 'user_id', dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :users, only: %i[index ]
+  resources :splashes, only: %i[index ]
   # Defines the root path route ("/")
-  root "splashes#index"
+  root to: "users#index"
+
 end
